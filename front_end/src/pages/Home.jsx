dbd1939319486eacfda73c96a313ce12cf76bd40
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-21 15:16:36 -0500

    Update Home.jsx

--- front_end/src/pages/Home.jsx
+++ front_end/src/pages/Home.jsx
@@ -28,7 +28,7 @@ function Home() {
     setLoading(true);
 
     try {
-        const response = await fetch( baseUrl+"/api/v1/post",
+        const response = await fetch( baseUrl+"/api/v1/post", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
