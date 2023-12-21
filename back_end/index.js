e665bff354556bf31d5eb410d054a73b28b9ca9c
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-21 17:45:18 -0500

    Update index.js

--- back_end/index.js
+++ back_end/index.js
@@ -11,15 +11,18 @@ dotenv.config();
 const app = express();
 app.use(cors({origin: '*'}));
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
+
+
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E')
 })
 
-app.use(express.static(path.join(__dirname, 'build')));
+
 
     app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
