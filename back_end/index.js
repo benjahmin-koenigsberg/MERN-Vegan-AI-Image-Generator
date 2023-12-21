c48002753c8fbd847ac0210906f19942ee08cd5c
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-21 13:01:16 -0500

    Update index.js
    adds app.use(cors({origin: '*'}));

--- back_end/index.js
+++ back_end/index.js
@@ -8,7 +8,7 @@ import dalleRoutes from './routes/dalleRoutes.js'
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes)
