fa817f1ad030eaba97ab9aee75c4b0a7833379b3
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2024-01-07 21:35:34 -0500

    Update dalleRoutes.js

--- back_end/routes/dalleRoutes.js
+++ back_end/routes/dalleRoutes.js
@@ -19,34 +19,59 @@ router.route('/').get((req, res) => {
     res.send('Hello from DALL-E')
 })
 
+// router.route('/').post(async (req, res) => {
+//     // const veganPromptAdds = ['vegan', 'animal rights', 'animal liberation' , 'vegan activist', 'animal liberation front', 'veganism', 'animal defender']
+//     // const randomAdd = veganPromptAdds[Math.floor(Math.random()*veganPromptAdds.length)];
+
+//     try {
+//         const { prompt } = req.body
+//         prompt+=' veganism'
+
+//         // const response = await axios.get(
+//         //     `https://lexica.art/api/v1/search?q=${prompt}`
+//         // );
+//         // const image = response.data.images[0].src
+//         // res.status(200).json({ photo: image });
+
+
+//         const aiResponse = await openai.images.generate({
+//             model: "dall-e-3",
+//             prompt,
+//             n: 1,
+//             size: '1024x1024',
+//             response_format: 'b64_json',
+//         });
+//         const image = aiResponse.data[0].b64_json
+//         res.status(200).json({ photo : image });
+
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send(error);
+//     }
+// });
+
 router.route('/').post(async (req, res) => {
-    // const veganPromptAdds = ['vegan', 'animal rights', 'animal liberation' , 'vegan activist', 'animal liberation front', 'veganism', 'animal defender']
-    // const randomAdd = veganPromptAdds[Math.floor(Math.random()*veganPromptAdds.length)];
-
     try {
-        const { prompt } = req.body 
-        prompt+=' veganism'
+        const { prompt } = req.body;
 
-        // const response = await axios.get(
-        //     `https://lexica.art/api/v1/search?q=${prompt}`
-        // );
-        // const image = response.data.images[0].src
-        // res.status(200).json({ photo: image });
 
 
         const aiResponse = await openai.images.generate({
             model: "dall-e-3",
+            // prompt: prompt + ' veganism',
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
         });
-        const image = aiResponse.data[0].b64_json
-        res.status(200).json({photo:image});
 
+        const image = aiResponse.data[0].b64_json;
+        res.status(200).json({ photo: image });
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
+
+        // res.status(500).send(error?.response.data.error.message || 'Something went wrong');
     }
 });
 
