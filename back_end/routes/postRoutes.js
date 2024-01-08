a254190ee44adb20340a82c5e3c6abdf9b460552
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2024-01-08 08:50:11 -0500

    Update postRoutes.js
    adds folder option in cloudinary upload

--- back_end/routes/postRoutes.js
+++ back_end/routes/postRoutes.js
@@ -28,7 +28,7 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
     try {
         const { name, prompt, photo } = req.body;
-        const photoUrl = await cloudinary.uploader.upload(photo);
+        const photoUrl = await cloudinary.uploader.upload(photo, { folder: "veganAI" });
 
 
         const newPost = await Post.create({
