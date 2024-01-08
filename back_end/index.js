7a98b8ce8069488f4e1de753ad7b6deea8e47889
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2024-01-07 20:00:09 -0500

    Update index.js
    removes arg from cors mifddleware

--- back_end/index.js
+++ back_end/index.js
@@ -9,7 +9,9 @@ import * as path from 'path'
 dotenv.config();
 
 const app = express();
-app.use(cors({origin: '*'}));
+// app.use(cors({origin: '*'}));
+app.use(cors());
+
 app.use(express.json({ limit: '50mb' }));
 
 
